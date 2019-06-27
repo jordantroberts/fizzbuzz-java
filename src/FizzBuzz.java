f3a4f84aa97ba060c5459e69a4c13521e703add9
Jordan Roberts <jordanroberts@talk21.com>
2019-06-27 16:19:04 +0100

    All tests passed

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,10 +1,13 @@
 public class FizzBuzz {
 
     public String isDivisibleBy(int a) {
-        if (a % 15 == 0){
+        if ( a % 15 == 0){
             return("Fizzbuzz");
-        } else if (a % 5 == 0);{
+        } else if (a % 5 == 0){
             return ("Buzz");
+        } else if (a % 3 == 0){
+            return("Fizz");
         }
+        return String.valueOf(a);
     }
 }
